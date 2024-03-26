78cc79b439109f9f02d5d795e51f6efa0aebae3c
remayhugh <remayhugh@mail.lipscomb.edu>
2024-03-25 20:26:20 -0500

    updated

--- moveToCan.m
+++ moveToCan.m
@@ -6,14 +6,13 @@
 
 %% Inputs
 
-masterhostIP = '192.168.64.129';
-port = 11311;
-vmIP = '10.51.84.223';
+% masterhostIP = withheld for security;
+% port = withheld for security;
+% machineIP = withheld for security;
 
 
 %% Preliminaries
 
-%rosinit(masterhostIP,11311,'NodeHost','10.51.84.223')
 rosinit(masterhostIP,port,'NodeHost',vmIP)
 % quick comment here: when you run this code, you will need to put in your
 % own port, IP, and vm IP that is running your gazebo.
@@ -21,15 +20,57 @@ rosinit(masterhostIP,port,'NodeHost',vmIP)
 
 %% Outputs
 
-% Controlling the Robot Arm Pose using the ROS Action Client
-% trajAct = rosactionclient('/pos_joint_traj_controller/follow_joint_trajectory','control_msgs/FollowJointTrajectory')
-% trajGoal = rosmessage(trajAct);
-% trajAct.FeedbackFcn = []; 
-% jointSub = rossubscriber("/joint_states")
-% jointStateMsg = jointSub.LatestMessage
+Controlling the Robot Arm Pose using the ROS Action Client
+trajAct = rosactionclient('/pos_joint_traj_controller/follow_joint_trajectory','control_msgs/FollowJointTrajectory');
+trajGoal = rosmessage(trajAct);
+trajAct.FeedbackFcn = []; 
+jointSub = rossubscriber("/joint_states");
+%jointStateMsg = jointSub.LatestMessage;
 
+%initalize robot
+UR5e = loadrobot('universalUR5e', DataFormat="row");
 
+tform=UR5e.Bodies{3}.Joint.JointToParentTransform;    
+UR5e.Bodies{3}.Joint.setFixedTransform(tform*eul2tform([pi/2,0,0]));
 
+tform=UR5e.Bodies{4}.Joint.JointToParentTransform;
+UR5e.Bodies{4}.Joint.setFixedTransform(tform*eul2tform([-pi/2,0,0]));
+
+tform=UR5e.Bodies{7}.Joint.JointToParentTransform;
+UR5e.Bodies{7}.Joint.setFixedTransform(tform*eul2tform([-pi/2,0,0]));
+
+ik = inverseKinematics("RigidBodyTree",UR5e); % Create Inverse kinematics solver
+ikWeights = [0.25 0.25 0.25 0.1 0.1 .1]; % configuration weights for IK solver [Translation Orientation] see documentation
+jointStateMsg = receive(jointSub,3); % receive current robot configuration
+initialIKGuess = homeConfiguration(UR5e);
+jointStateMsg.Name
+initialIKGuess(1) = jointStateMsg.Position(4); % update configuration in initial guess
+initialIKGuess(2) = jointStateMsg.Position(3);
+initialIKGuess(3) = jointStateMsg.Position(1);
+initialIKGuess(4) = jointStateMsg.Position(5);
+initialIKGuess(5) = jointStateMsg.Position(6);
+initialIKGuess(6) = jointStateMsg.Position(7);
+show(UR5e,initialIKGuess)
+
+gripper1 = -0.54;
+gripper2 = 0.15;
+gripper3 = 0.22;
+
+gripperTranslation = [gripper1 gripper2 gripper3];
+gripperRotation = [-pi/2 -pi 0]; %  [Z Y Z] radians
+tform = eul2tform(gripperRotation); % ie eul2tr call
+tform(1:3,4) = gripperTranslation'; % set translation in homogeneous transform
+[configSoln, solnInfo] = ik('tool0',tform,ikWeights,initialIKGuess);
+%show(UR5e,configSoln)
+
+UR5econfig = [configSoln(3)... 
+              configSoln(2)...
+              configSoln(1)...
+              configSoln(4)...
+              configSoln(5)...
+              configSoln(6)];
+trajGoal = packTrajGoal(UR5econfig,trajGoal);
+sendGoal(trajAct,trajGoal)
 
 
 
